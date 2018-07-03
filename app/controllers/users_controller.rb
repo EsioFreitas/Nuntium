dc33ef9a023c6bb9ebd3d7947b1b44228f814f42
Esio Freitas <esio.gustavo@gmail.com>
2018-07-03 01:13:32 -0300

    Adding the font ans custom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,20 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
     def index
       @users = User.all
     end
      def show
-      @user = User.find(params[:id])
      end
+
+     private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tag_params
+      params.require(:user).permit(:name, :email, :about, :articles_id)
+    end
   end
